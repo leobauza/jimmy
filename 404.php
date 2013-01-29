f68edeaaf5923b50141ee800236a9dbf67ccc045
Leo Bauza <lb@hy.ly>
2013-01-29 00:35:29 -0500

    404 caged

--- 404.php
+++ 404.php
@@ -5,7 +5,18 @@
 	<section class="page-wrap">
 		<?php include (TEMPLATEPATH . '/inc/loadoverlay.php' ); ?>
 		<div class="page-cont">
-			<h2>Error 404 - Page Not Found</h2>
+			<section id="hero" class="group" data-margin="60">
+				<figure class="feat-img">
+					<img src="<?php bloginfo('template_url'); ?>/img/error_404.jpg" alt="Error 404">
+				</figure>
+				<aside class="feat-text">
+					<article class="summary">
+						<h2>Error 404 - Page Not Found</h2>
+						<p>You've made a terrible mistake coming here...please go away. Hopefully this Image makes Billy come up with a real 404 page error image.</p>
+						<a href="#filter" class="more-btn">Close this terrible error</a>
+					</article>
+				</aside>
+			</section>
 		</div>
 		<!-- end page content -->
 	</section>
@@ -14,6 +25,7 @@
 
 
 <?php get_footer(); ?>
+
 
 
 
