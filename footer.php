8da43b53f707ecef2f5da298954fcd0144a36ba8
Leo Bauza <lb@hy.ly>
2013-01-29 15:40:39 -0500

    back button working added new functions to footer

--- footer.php
+++ footer.php
@@ -54,24 +54,56 @@
 					$('.page-cont [data-margin] .summary').attr('style','margin:20px 15px');
 				}
 			}); // end slide down
+			//check internals after ajax
+			$(".more-btn").each(function(){
+				var
+					$this = $(this),
+					url = $this.attr('href')||''
+				;
+				//check links
+				if(url.substring(0,rootUrl.length) === rootUrl) {//|| url.indexOf(':') === -1) {
+					$this.addClass('internal')
+				}
+			}); //end each function
 		} // end load new page
 
+		function updateMenu(relativeUrl, url){
+			// Update the menu
+			$menuChildren = $menu.find(menuChildrenSelector);
+			$menuChildren.filter(activeSelector).removeClass(activeClass);
+			$menuChildren = $menuChildren.has('a[href^="'+relativeUrl+'"],a[href^="/'+relativeUrl+'"],a[href^="'+url+'"]');
+			if ( $menuChildren.length === 1 ) { $menuChildren.addClass(activeClass); }
+		}
+
+		function googleAnalytics() {
+			// update google analytics here
+			// if ( typeof window.pageTracker !== 'undefined' ) {
+			//	window.pageTracker._trackPageview(relativeUrl);
+			//	//or for the newer tracking code
+			//	_gaq.push(['_trackPageview', relativeUrl]);
+			// }
+			
+		}
 
 		// Bind to StateChange Event
 		History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
 			var 
-				State = History.getState(), // Note: We are using History.getState() instead of event.state
-				stateData = State.data
-			;
-			console.log(State);
-			var url = State.url;
+				State = History.getState(); // Note: We are using History.getState() instead of event.state
 			
 			if(clickEvent == 0) {
-				var	 pageRequest = $.ajax({
+				//animation and set up url for request
+				scroll(0,0); //scrollTo(0,0);
+				$('.loading').show();
+				$('.load-overlay').fadeIn();
+				$('.page-cont').slideUp();
+				var 
+					url = State.url;
+					relativeUrl = url.replace(rootUrl,'');
+				;
+				
+				var pageRequest = $.ajax({
 					url: url,
 					success: function(data){
-						console.log('state change event ajax');
-
 						//prepare
 						var
 							$data = $(documentHtml(data)),
@@ -80,12 +112,11 @@
 						;
 						$('.page-cont').html($newPage);
 
-						// update google analytics here
-						// if ( typeof window.pageTracker !== 'undefined' ) {
-						//	window.pageTracker._trackPageview(relativeUrl);
-						//	//or for the newer tracking code
-						//	_gaq.push(['_trackPageview', relativeUrl]);
-						// }
+						//updates the active link
+						updateMenu(relativeUrl, url);
+						//triggers google analytics tracking
+						googleAnalytics();
+						//animates the new page open
 						loadNewPage();
 					}
 				}); //end ajax
@@ -127,9 +158,19 @@
 		var clickEvent = 0;
 		//navigation click
 		$("a.internal").live("click",function(e){
+			//dont fire the code in statechange function
 			clickEvent = 1;
+			scroll(0,0); //scrollTo(0,0);
+			$('.loading').show();
+			$('.load-overlay').fadeIn();
+			$('.page-cont').slideUp();
+			//set up the url for ajax and updating menu
+			var 
+				url = $(this).attr('href'),
+				relativeUrl = url.replace(rootUrl,'');
+			;
+			
 			//request that page, greg style
-			console.log(url);
 			var pageRequest = $.ajax({
 				url: url,
 				success: function(data){
@@ -142,69 +183,52 @@
 						$newPage = $data.find(".page-cont").html(),
 						$menuChildren
 					;
-					// Update the menu
-					$menuChildren = $menu.find(menuChildrenSelector);
-					$menuChildren.filter(activeSelector).removeClass(activeClass);
-					$menuChildren = $menuChildren.has('a[href^="'+relativeUrl+'"],a[href^="/'+relativeUrl+'"],a[href^="'+url+'"]');
-					if ( $menuChildren.length === 1 ) { $menuChildren.addClass(activeClass); }
+
+					updateMenu(relativeUrl,url);
 
 					$('.page-cont').html($newPage);
-
-					// update google analytics here
-					// if ( typeof window.pageTracker !== 'undefined' ) {
-					//	window.pageTracker._trackPageview(relativeUrl);
-					//	//or for the newer tracking code
-					//	_gaq.push(['_trackPageview', relativeUrl]);
-					// }
-
+					googleAnalytics();
 					History.pushState(null, $title, url); // uh? change title and update url
 
 
 
 				}
 			}); //end ajax
-			
-			
-			scroll(0,0); //scrollTo(0,0);
-			$('.loading').show();
-			$('.load-overlay').fadeIn();
-			$('.page-cont').slideUp();
-			var 
-				url = $(this).attr('href'),
-				relativeUrl = url.replace(rootUrl,'');
-			;
+
 			//after a succesful page request rock out
 			pageRequest.done(function(msg){
 				console.log("page request done");
-				$('.shadow').css('top','100%');
-				$('.page-cont').delay(500).slideDown(750, function(){
-					$('.load-overlay').fadeOut();
-					$('.loading').hide();
-					//add approrpiate centering for page area
-					if($winSize >= 980) {
-						if($('.page-cont [data-margin]').length) {
-							var
-								modMar = parseFloat($('.page-cont [data-margin]').attr('data-margin')),
-								imgheight = $('.page-cont img').height()
-							;
-							$('.page-cont [data-margin]').applyMargins(modMar, imgheight);
-						}
-					} else {
-						$('.page-cont [data-margin] .summary').attr('style','margin:20px 15px');
-					}
-				}); // end slide down 
-
-				//check internals after ajax
-				$(".more-btn").each(function(){
-					var
-						$this = $(this),
-						url = $this.attr('href')||''
-					;
-					//check links
-					if(url.substring(0,rootUrl.length) === rootUrl) {//|| url.indexOf(':') === -1) {
-						$this.addClass('internal')
-					}
-				}); //end each function
+				//load new page does everything commented out below
+				loadNewPage();
+				// $('.shadow').css('top','100%');
+				// $('.page-cont').delay(500).slideDown(750, function(){
+				// 	$('.load-overlay').fadeOut();
+				// 	$('.loading').hide();
+				// 	//add approrpiate centering for page area
+				// 	if($winSize >= 980) {
+				// 		if($('.page-cont [data-margin]').length) {
+				// 			var
+				// 				modMar = parseFloat($('.page-cont [data-margin]').attr('data-margin')),
+				// 				imgheight = $('.page-cont img').height()
+				// 			;
+				// 			$('.page-cont [data-margin]').applyMargins(modMar, imgheight);
+				// 		}
+				// 	} else {
+				// 		$('.page-cont [data-margin] .summary').attr('style','margin:20px 15px');
+				// 	}
+				// }); // end slide down 
+				// 
+				// //check internals after ajax
+				// $(".more-btn").each(function(){
+				// 	var
+				// 		$this = $(this),
+				// 		url = $this.attr('href')||''
+				// 	;
+				// 	//check links
+				// 	if(url.substring(0,rootUrl.length) === rootUrl) {//|| url.indexOf(':') === -1) {
+				// 		$this.addClass('internal')
+				// 	}
+				// }); //end each function
 			}); //end page request
 			e.preventDefault();
 		}); //end click
