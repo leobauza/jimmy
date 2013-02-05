3a2f0e28ff6131fd714120f711725b8c0da25994
Leo Bauza <lb@hy.ly>
2013-02-04 23:27:22 -0500

    commented out g-analytics until I write something for local google analytics ignore..

--- footer.php
+++ footer.php
@@ -108,7 +108,7 @@
 						//if ( typeof window.pageTracker !== 'undefined' ) {
 							//window.pageTracker._trackPageview(relativeUrl);
 							//or for the newer tracking code
-							_gaq.push(['_trackPageview', relativeUrl]);
+							//_gaq.push(['_trackPageview', relativeUrl]);
 						//}
 
 						//animates the new page open
@@ -186,7 +186,7 @@
 					//if ( typeof window.pageTracker !== 'undefined' ) {
 						//window.pageTracker._trackPageview(relativeUrl);
 						//or for the newer tracking code
-						_gaq.push(['_trackPageview', relativeUrl]);
+						//_gaq.push(['_trackPageview', relativeUrl]);
 					//}
 
 					History.pushState(null, $title, url); // uh? change title and update url
@@ -260,14 +260,14 @@
 
 
 	<script type="text/javascript">
-		var _gaq = _gaq || [];
-		_gaq.push(['_setAccount', 'UA-38051085-1']);
-		_gaq.push(['_trackPageview']);
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
+		// var _gaq = _gaq || [];
+		// _gaq.push(['_setAccount', 'UA-38051085-1']);
+		// _gaq.push(['_trackPageview']);
+		// (function() {
+		// 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		// 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		// 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		// })();
 
 	</script>
 
